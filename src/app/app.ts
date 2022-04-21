bc0ef6fbe58b691aa90950ac19dc6657d35af06a
mandidisha <mandidisha@gmail.com>
2022-04-21 13:23:18 +0200

    user and airplane implementation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import bodyParser from 'body-parser';
-// import Path from 'path';
-// import YAML from 'yamljs';
+import Path from 'path';
+import YAML from 'yamljs';
 import SwaggerUI from 'swagger-ui-express';
-// import SwaggerJsdoc from 'swagger-jsdoc';
+import SwaggerJsdoc from 'swagger-jsdoc';
 import express from 'express';
 import mongoose from 'mongoose';
-import Passport from 'passport';
 import Http from 'http';
-import { jwtAuth } from './authentication';
+import { basicAuth, jwtAuth } from './authentication';
+import routes from './modules/routes';
 import config from './providers/development';
 
 const app = express();
@@ -21,38 +21,30 @@ mongoose.connect(config.databaseUrl, {
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send(req.body);
 });
+app.use(express.json);
+app.use('api/v1', routes);
 // Setup docs
-// const docsFilePath = Path.resolve(__dirname, '../docs/openapi.yaml');
-// const jsonDocsFile = YAML.load(docsFilePath);
-// const docs = SwaggerJsdoc({
-//   swaggerDefinition: jsonDocsFile,
-//   apis: ['./src/app/**/*.js'],
-// });
-
-// app.use(
-//   '/api/swagger',
-//   basicAuth(config.apiDocsUsername, config.apiDocsPassword!, true),
-//   SwaggerUI.serve,
-//   SwaggerUI.setup(docs, false!, { docExpansion: 'none' }),
-// );
+const docsFilePath = Path.resolve(__dirname, './docs/openapi.yaml');
+const jsonDocsFile = YAML.load(docsFilePath);
+const docs = SwaggerJsdoc({
+  swaggerDefinition: jsonDocsFile,
+  apis: ['./src/app/**/*.ts'],
+});
 
 app.use(
-  '/docs',
+  '/api/swagger',
+  // basicAuth(config.apiDocsUsername, config.apiDocsPassword!, true),
   SwaggerUI.serve,
-  SwaggerUI.setup(undefined, {
-    swaggerOptions: {
-      url: '/swagger.json',
-    },
-  }),
+  SwaggerUI.setup(docs),
 );
 
 app.get('/help', (req: express.Request, res: express.Response) => {
   res.send('help page');
 });
+
 jwtAuth();
 app.use(bodyParser.json());
 
-app.use(Passport.initialize());
 server.listen(config.port, () => console.log(`app running on port ${config.port}`));
 
 export default server;
